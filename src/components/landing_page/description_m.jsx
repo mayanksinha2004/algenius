fcc39b48cd3e9df56bd9883b8dae0e5a82d94f5a
mayanksinha2004 <f20212078@pilani.bits-pilani.ac.in>
2024-12-05 09:36:49 +0530

    description margin sorted

--- src/components/landing_page/description_m.jsx
+++ src/components/landing_page/description_m.jsx
@@ -6,7 +6,7 @@ const Description_M= () => {
       {/* H1 Wrapper */}
       <div className="flex-none w-full  h-[5.49vh]  border-solid border-[#2B1B38] box-border flex items-center justify-center  flex-none order-2 flex-grow-0  border-t-[1px] border-r-[1px] border-b-[0px] border-l-[1px] border-solid border-[#2B1B38]">
         {/* AIgenius Text */}
-        <p className="text-center font-[DM Sans] font-normal text-[14px] leading-[128%] text-[#D387FF] w-[384px] max-w-[384px] h-[46px] -tracking-[0.02em] z-0">
+        <p className="text-center font-[DM Sans] font-normal text-[14px] leading-[128%] text-[#D387FF] w-[340px]  h-[46px] -tracking-[0.02em] z-0">
           Be first in line to experience the future of AI with AIgenius! Join our waitlist for exclusive access.
         </p>
       </div>
